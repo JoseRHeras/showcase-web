b3cc983d2ab6c7bd22c130ae322ff968677604cc
Jose Heras <jrheras@yahoo.com>
2021-09-24 21:19:50 -0500

    base.html has been modified to use templatetag to insert a navbar

--- web_portfolio/landing_page/templatetags/links_loader.py
+++ web_portfolio/landing_page/templatetags/links_loader.py
@@ -1,10 +1,25 @@
+from typing import final
 from django import template
 from landing_page.models import SocialPlatforms
 
 register = template.Library()
 
 
-@register.inclusion_tag('base/footer.html')
+@register.inclusion_tag('base/footer.html', name='footer')
 def social_media_footer():
+
     links = SocialPlatforms.objects.all()
     return {'links' : links}
+
+
+
+@register.inclusion_tag('base/navbar.html', name="navbar", takes_context=True)
+def navbar(context):
+    path = context.request.path
+    navbar_active = {
+        'home':  'active'if path == '/' else '',
+        'project': 'active'if path == '/projects' else '',
+        'resume': 'active'if path == '/resume' else ''
+    }
+
+    return {'links': navbar_active}
